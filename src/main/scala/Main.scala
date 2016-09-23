5befc3e6382378cb0a167af56b3305376e0b5deb
BartoszB <bartosz.babol@agoda.com>
2016-09-24 04:08:52 +0700

    Parsing code

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,21 @@
 import scala.meta._
 
 object Main extends App{
+  val code = """val a: List[String]= List()""".parse[Stat]
+  val caseExpr = """case true => println("its true!")""".parse[Case]
+  val term = """x + y""".parse[Term]
+  val arg = """a: List[String]""".parse[Term.Arg]
 
+  printResult[Stat](code)
+  printResult[Case](caseExpr)
+  printResult[Term](term)
+  printResult[Term.Arg](arg)
+
+  def printResult[T](code: Parsed[T]): Unit = {
+    code match {
+      case Parsed.Success(tree) => println("Code is valid!")
+      case Parsed.Error(pos, msg, details)  =>
+        println(s"Pos: $pos, msg: $msg. More details: $details")
+    }
+  }
 }

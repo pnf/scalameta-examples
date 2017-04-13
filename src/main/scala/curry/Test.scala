d653e9f71769b6e688efc5b9a237449f316bbd90
Peter Fraenkel <pnf@podsnap.com>
2017-04-13 16:25:55 -0400

    overambitious currying

--- src/main/scala/curry/Test.scala
+++ src/main/scala/curry/Test.scala
@@ -3,11 +3,21 @@ package curry
 /**
   * Created by pnf on 4/13/2017.
   */
+class Bar {
+  def apply(i: Int): Int => Int
+}
+
 object Test extends App {
-  @autoCurryMethods object Foo {
-    def foo(i: Int, j: Int, k: Int) = i + j + k
+  // @autoCurryMethods
+  object Foo {
+    def foo(i: Int, j: Int, k: Int): Int = i + j + k
+    def foo(i: Int, j: Int)(k: Int) = i + j + k
+    def foo(i: Int) = new Bar { def apply(j: Int) = (k: Int) => i + j + k}
   }
 
-  println(Foo.foo(1)(2)(3))
+//  println(Foo.foo(1)(2)(3))
+//  println(Foo.foo(1,2)(3))
+  println(Foo.foo(1,2,3)) // Function3[Int x 4]   ... Function3[,,,]
+  println(Foo.foo(1,2)(3))  // Function2[Int,Int,Function1[Int,Int]]  Function2[,,]
 }
 
--- src/main/scala/curry/package.scala
+++ src/main/scala/curry/package.scala
@@ -11,11 +11,24 @@ package object curry {
           case q"..$mods object $name extends $template" =>
             val template"{ ..$statsEarly } with ..$ctorcalls { $param => ..$statsBody }" = template
             val statsBody2 = statsBody.flatMap { _ match {
-              case x@q"..$mods def $name[..$tparams](...$paramss): $tpeopt = $expr" if paramss.size==1 && paramss.head.size>1 =>
-                val paramsc = paramss.head.dropRight(1).map(List(_))
+              case orig@q"..$mods def $name[..$tparams](...$paramss): $tpeopt = $expr" if paramss.size==1 && paramss.head.size>1 =>
                 val rp = paramss.last.last :: Nil
+                val param"..$mods $paramname: $atpeopt = $expropt" = paramss.last.last
+                println(s"..$mods $paramname: $atpeopt = $expropt")
+                val ftype = for(lp <- atpeopt;
+                    rp <- tpeopt;
+                     lps = List(lp)) yield t"(..$lps) => $rp"
                 val expr2 = q"(..$rp) => $expr"
-                List(x,q"..$mods def $name[..$tparams](...$paramsc): $tpeopt = {$expr2}")
+                val params = paramss.head.dropRight(1)
+                val curried = for(n <- 1 to params.size) yield {
+                  val (p1,r) = params.splitAt(n) //   [1,2], [3]
+                  val pcs = r.map(List(_)) // [[3][4]]
+                  val paramss2 = p1 +: pcs  // (1,2)(3)(4)
+                  q"..$mods def $name[..$tparams](...$paramss2): $ftype = {$expr2}"
+                }
+                val res = orig +: curried
+                println(res)
+                res
               case other => List(other)
             }}
             val template2 = template"{ ..$statsEarly } with ..$ctorcalls { $param => ..$statsBody2 }"

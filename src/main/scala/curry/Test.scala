595629135549d3413c0cdb30ec7577e2374a7f87
Peter Fraenkel <pnf@podsnap.com>
2017-04-14 11:10:04 -0400

    Add documentation; temporarily (hopefully) omit partially curried methods

--- src/main/scala/curry/Test.scala
+++ src/main/scala/curry/Test.scala
@@ -3,21 +3,19 @@ package curry
 /**
   * Created by pnf on 4/13/2017.
   */
-class Bar {
-  def apply(i: Int): Int => Int
-}
 
 object Test extends App {
-  // @autoCurryMethods
+  @autoCurryMethods
   object Foo {
     def foo(i: Int, j: Int, k: Int): Int = i + j + k
-    def foo(i: Int, j: Int)(k: Int) = i + j + k
-    def foo(i: Int) = new Bar { def apply(j: Int) = (k: Int) => i + j + k}
+    def bar(i: Int, j:Int) = i + j
   }
+
+
 
 //  println(Foo.foo(1)(2)(3))
 //  println(Foo.foo(1,2)(3))
   println(Foo.foo(1,2,3)) // Function3[Int x 4]   ... Function3[,,,]
-  println(Foo.foo(1,2)(3))  // Function2[Int,Int,Function1[Int,Int]]  Function2[,,]
+  println(Foo.foo(1)(2)(3))  // Function2[Int,Int,Function1[Int,Int]]  Function2[,,]
 }
 
--- src/main/scala/curry/package.scala
+++ src/main/scala/curry/package.scala
@@ -11,29 +11,44 @@ package object curry {
           case q"..$mods object $name extends $template" =>
             val template"{ ..$statsEarly } with ..$ctorcalls { $param => ..$statsBody }" = template
             val statsBody2 = statsBody.flatMap { _ match {
+              // Match on method definition with exactly one parameter group, containing more than one parameter
               case orig@q"..$mods def $name[..$tparams](...$paramss): $tpeopt = $expr" if paramss.size==1 && paramss.head.size>1 =>
-                val rp = paramss.last.last :: Nil
-                val param"..$mods $paramname: $atpeopt = $expropt" = paramss.last.last
-                println(s"..$mods $paramname: $atpeopt = $expropt")
-                val ftype = for(lp <- atpeopt;
-                    rp <- tpeopt;
-                     lps = List(lp)) yield t"(..$lps) => $rp"
-                val expr2 = q"(..$rp) => $expr"
+                // Last parameter of first (and only group)
+                val lp = paramss.head.last
+                val lps = lp :: Nil
+                // Destructure last parameter
+                val param"..$mods $paramname: $atpeopt = $expropt" = lp
+                // Construct type for function from last parameter type to method result type
+                val ftype = for(lpt <- atpeopt;
+                                rpt <- tpeopt;
+                                lpts = List(lpt)) yield t"(..$lpts) => $rpt"
+                // Construct lambda from last parameter to result
+                val fexpr = q"(..$lps) => $expr"
                 val params = paramss.head.dropRight(1)
+                // Build list of curried forms.  E.g starting with f(i,j,k,l) = expr
+                // construct
+                //   f(i)(j)(k) = l => expr
+                //   f(i,j)(k) = l => expr
                 val curried = for(n <- 1 to params.size) yield {
                   val (p1,r) = params.splitAt(n) //   [1,2], [3]
                   val pcs = r.map(List(_)) // [[3][4]]
                   val paramss2 = p1 +: pcs  // (1,2)(3)(4)
-                  q"..$mods def $name[..$tparams](...$paramss2): $ftype = {$expr2}"
+                  val c = q"..$mods def $name[..$tparams](...$paramss2): $ftype = {$fexpr}"
+                  // Uhm, how do we generate official compiler INFO?
+                  println(s"Curried: $c")
+                  c
                 }
-                val res = orig +: curried
-                println(res)
+                // This is exciting, but for now we need to throw out all but fully curried form due to
+                // type erasure issues.
+                //val res = orig +: curried
+                val res = orig :: curried.head :: Nil
                 res
               case other => List(other)
             }}
             val template2 = template"{ ..$statsEarly } with ..$ctorcalls { $param => ..$statsBody2 }"
             q"..$mods object $name extends $template2"
           case _ =>
+            // How do we generate ERROR?
             throw new RuntimeException
             null
         }
